c4dbe02b8de448a44d49523500bacf0961f659b2
Sabry Razick <sabryr@gmail.com>
2020-04-15 22:32:20 +0200

    Debug prints

--- R/PMreport.R
+++ R/PMreport.R
@@ -528,8 +528,11 @@ makeRdata <- function(wd, remote, reportType = 1) {
   #error <- length(errfile) > 0
   #see if NP_RF or IT_RF made anyway (i.e. is >1MB in size)
   success <- file.info(c("NP_RF0001.TXT", "IT_RF0001.TXT")[reportType])$size >= 1000
-  print("success 2")
+  print("success 2 ")
   print(success)
+  print("reportType")
+  print(reportType)
+
   if (success) {
     #run completed
     #open and parse the output
