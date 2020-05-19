a7d79da030b5d949fd4dcd68611705427c3ebd8a
Sabry Razick <sabryr@gmail.com>
2020-05-19 17:18:25 +0200

    Debug print

--- R/NPrun.R
+++ R/NPrun.R
@@ -95,7 +95,7 @@ NPrun <- function(model = "model.txt", data = "data.csv", run,
                   idelta = 12, prior,
                   auto = T, intern = F, silent = F, overwrite = F, nocheck = F, parallel = NA, 
                   alq = F, remote = F, server_address) {
-
+   print(paste("ode =", ode, "  tol = ", tol))
   if (missing(run)) run <- NULL
   if (missing(include)) include <- NULL
   if (missing(exclude)) exclude <- NULL
